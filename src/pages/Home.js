ba506b27280253eb51d85de132baaacf1c24fc6c
deeppaz <deeppaz@hotmail.com>
2019-12-16 23:58:47 +0300

    edit home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Logo from '../img/logo.png';
+import ForHome from '../components/ForHome';
 
 //grommet işlevleri
-import { Box, Grommet, Stack, Image } from "grommet";
+import {
+  Box,
+  Grommet,
+  Stack,
+  Image,
+  Button
+} from "grommet";
+
+import { Next } from "grommet-icons";
+
 
 const Wrapper = styled.section`
-  padding-left: 40%;
+  padding-right: 50%;
+`;
+
+const Wrapper2 = styled.section`
+  padding-left: 50%;
+  margin-top: -6%;
 `;
 
 const Body = styled.section`
-  padding: 5%;
+  background-color: gainsboro;
 `;
+
 
 class Home extends Component {
   render() {
@@ -18,13 +35,37 @@ class Home extends Component {
       <Grommet full>
         <Body>
           <Stack fill>
-            <Wrapper>
-              <Image fit="contain" src="https://i.hizliresim.com/YdoA8Z.png" />
-            </Wrapper>
-            <Box background="dark" fill>
-              Test
+            {/* navbar */}
+            <Box
+              align="center"
+              pad="large"
+            >
+              <Wrapper>
+                <Image fit="contain" src={Logo} style={{ width: '40%' }} />
+              </Wrapper>
+              <Wrapper2>
+                <Button primary label="Hemen Başla!" icon={<Next />} />
+              </Wrapper2>
             </Box>
+            {/* navbar end */}
           </Stack>
+          <Box
+            direction="row-responsive"
+            justify="center"
+            align="center"
+            pad="xlarge"
+            gap="medium"
+          >
+            <Box
+              pad="large"
+              align="center"
+              background={{ color: "light-2", opacity: "strong" }}
+              round
+              gap="small"
+            >
+              <ForHome/>
+            </Box>
+          </Box>
         </Body>
       </Grommet>
     )
