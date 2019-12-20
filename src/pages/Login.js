9b15e426236cda43bccbd70f392f62e96edcee40
deeppaz <deeppaz@hotmail.com>
2019-12-20 23:20:14 +0300

    update login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Logo from '../img/logo.png';
 
-export class Login extends Component {
+import { grommet, Box, FormField, Form, Text, Heading, Button, Grommet, Image } from "grommet";
+
+const FormFieldLabel = props => {
+    const { required, label, ...rest } = props;
+    return (
+        <FormField
+            label={
+                required ? (
+                    <Box direction="row">
+                        <Text>{label}</Text>
+                        <Text color="status-critical">*</Text>
+                    </Box>
+                ) : (
+                        label
+                    )
+            }
+            required={required}
+            {...rest}
+        />
+    );
+};
+
+class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Hey im login
-                </h1>
-            </div>
+            <Grommet theme={grommet}>
+                <Box align="center" pad="large">
+                    <Box height="small" width="small">
+                        <Image
+                            fit="contain"
+                            src={Logo}
+                        />
+                    </Box>
+                    <Heading color="accent-4">Giriş Yap</Heading>
+                    <Form>
+                        <FormFieldLabel name="firstName" label="Adınız" required />
+                        <FormFieldLabel name="password" label="Şifre" required />
+                        <FormFieldLabel name="email" label="Email" />
+                        <Button type="submit" label="Submit" primary />
+                        <Text margin={{ left: "small" }} size="small" color="status-critical">
+                            * Isteniliyor
+                        </Text>
+                    </Form>
+                </Box>
+            </Grommet>
         )
     }
 }
